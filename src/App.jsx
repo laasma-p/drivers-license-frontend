0edbc2f5a831cb5d2aad437c9d4a46cc2f8b77b1
Lasma Poksane <laasmapoksane@gmail.com>
2024-03-12 15:03:57 +0100

    render Quiz component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import "./App.css";
 import Authorization from "./components/Authorization";
 import Instructions from "./components/Instructions";
+import Quiz from "./containers/Quiz";
 
 function App() {
   return (
     <div className="min-h-screen bg-gray-100">
       <Authorization />
       <Instructions />
+      <Quiz />
     </div>
   );
 }
