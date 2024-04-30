9ccb6cd139a0f418e1b3ed815df4315ef4dadfb0
Lasma Poksane <laasmapoksane@gmail.com>
2024-04-30 13:11:54 +0200

    create initial Quiz component with rendered Timer and QuizCard

---
+++ src/containers/Quiz.jsx
@@ -0,0 +1,13 @@
+import QuizCard from "../components/QuizCard";
+import Timer from "../containers/Timer";
+
+const Quiz = () => {
+  return (
+    <>
+      <Timer />
+      <QuizCard />
+    </>
+  );
+};
+
+export default Quiz;
