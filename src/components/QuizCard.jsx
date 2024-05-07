bc2d5413b185bad5925e10970aa8454bdbfc2d31
Lasma Poksane <laasmapoksane@gmail.com>
2024-05-07 11:15:35 +0200

    re-design QuizCard component

--- src/components/QuizCard.jsx
+++ src/components/QuizCard.jsx
@@ -2,55 +2,46 @@ import image from "../assets/view.jpg";
 
 const QuizCard = () => {
   return (
-    <div className="flex w-11/12">
-      <div className="w-1/2">
-        <img src={image} alt="image" width="700px" height="auto" />
+    <div className="w-11/12">
+      <div>
+        <img src={image} alt="image" height="auto" />
       </div>
-      <div className="w-1/2 h-full">
-        <form className="p-4 h-full">
-          <div className="h-4/5">
-            <h2 className="text-xl">A question</h2>
-            <h3 className="text-lg pb-2">Statement 1</h3>
-            <div className="flex flex-row justify-between pb-3">
-              <div className="w-1/2 flex flex-row">
-                <input type="radio" />
-                <label className="p-2">Yes</label>
-              </div>
-              <div className="w-1/2 flex flex-row">
-                <input type="radio" />
-                <label className="p-2">No</label>
-              </div>
+      <form>
+        <div>
+          <h2 className="text-xl px-2 pt-1 pb-2">Question</h2>
+          <div className="flex flex-col gap-1">
+            <div className="text-lg px-1 flex items-center">
+              <input
+                type="checkbox"
+                id="statement1"
+                className="w-4 h-4 ml-1 mr-2 cursor-pointer"
+              />
+              <label htmlFor="statement1">Statement 1</label>
             </div>
-            <h3 className="text-lg pb-2">Statement 2</h3>
-            <div className="flex flex-row justify-between pb-3">
-              <div className="w-1/2 flex flex-row">
-                <input type="radio" />
-                <label className="p-2">Yes</label>
-              </div>
-              <div className="w-1/2 flex flex-row">
-                <input type="radio" />
-                <label className="p-2">No</label>
-              </div>
+            <div className="text-lg px-1 flex items-center">
+              <input
+                type="checkbox"
+                id="statement2"
+                className="w-4 h-4 ml-1 mr-2 cursor-pointer"
+              />
+              <label htmlFor="statement2">Statement 2</label>
             </div>
-            <h3 className="text-lg pb-2">Statement 3</h3>
-            <div className="flex flex-row justify-between pb-3">
-              <div className="w-1/2 flex flex-row">
-                <input type="radio" />
-                <label className="p-2">Yes</label>
-              </div>
-              <div className="w-1/2 flex flex-row">
-                <input type="radio" />
-                <label className="p-2">No</label>
-              </div>
+            <div className="text-lg px-1 flex items-center">
+              <input
+                type="checkbox"
+                id="statement3"
+                className="w-4 h-4 ml-1 mr-2 cursor-pointer"
+              />
+              <label htmlFor="statement3">Statement 3</label>
             </div>
           </div>
-          <div className="w-full h-1/5 flex justify-end items-end">
-            <button className="h-10 text-md sm:text-lg border-0 bg-sky-400 hover:bg-sky-700 transition-colors hover:text-gray-100 rounded-md px-6">
-              Next
-            </button>
-          </div>
-        </form>
-      </div>
+        </div>
+        <div>
+          <button className="w-full mt-4 text-gray-100 bg-sky-400 hover:bg-sky-700 py-2 transition-colors rounded-md">
+            Next
+          </button>
+        </div>
+      </form>
     </div>
   );
 };
