60572680bf75b1185cc4774e180a1f5f196e80cc
Lasma Poksane <laasmapoksane@gmail.com>
2024-05-07 12:31:49 +0200

    adjust Header component styles for different screens

--- src/containers/Header.jsx
+++ src/containers/Header.jsx
@@ -4,11 +4,11 @@ import Timer from "./Timer";
 const Header = () => {
   return (
     <header className="bg-sky-400 py-2">
-      <div className="flex mx-3 mx-2">
-        <div className="w-3/6">
+      <div className="flex mx-3 md:w-11/12 md:mx-auto">
+        <div className="w-3/6 md:w-1/6 flex items-center">
           <img src={logo} alt="logo" />
         </div>
-        <div className="w-3/6 flex items-center justify-end">
+        <div className="w-3/6 md:w-5/6 flex items-center justify-end">
           <Timer />
         </div>
       </div>
