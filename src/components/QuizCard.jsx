ded54f6299c7f64773660e48a8ae1faa6bee214c
Lasma Poksane <laasmapoksane@gmail.com>
2024-05-07 12:27:58 +0200

    adjust classes to different screen sizes

--- src/components/QuizCard.jsx
+++ src/components/QuizCard.jsx
@@ -2,12 +2,12 @@ import image from "../assets/view.jpg";
 
 const QuizCard = () => {
   return (
-    <div className="w-11/12 mt-3">
-      <div>
+    <div className="w-11/12 mt-3 md:flex max-w-7xl">
+      <div className="md:w-1/2">
         <img src={image} alt="image" height="auto" />
       </div>
-      <form>
-        <div>
+      <form className="md:w-1/2">
+        <div className="md:h-4/5">
           <h2 className="text-xl px-2 pt-1 pb-2">Question</h2>
           <div className="flex flex-col gap-1">
             <div className="text-lg px-1 flex items-center">
@@ -16,7 +16,9 @@ const QuizCard = () => {
                 id="statement1"
                 className="w-4 h-4 ml-1 mr-2 cursor-pointer"
               />
-              <label htmlFor="statement1">Statement 1</label>
+              <div className="w-fit">
+                <label htmlFor="statement1">Statement 1</label>
+              </div>
             </div>
             <div className="text-lg px-1 flex items-center">
               <input
@@ -24,7 +26,9 @@ const QuizCard = () => {
                 id="statement2"
                 className="w-4 h-4 ml-1 mr-2 cursor-pointer"
               />
-              <label htmlFor="statement2">Statement 2</label>
+              <div className="w-fit">
+                <label htmlFor="statement2">Statement 2</label>
+              </div>
             </div>
             <div className="text-lg px-1 flex items-center">
               <input
@@ -32,12 +36,14 @@ const QuizCard = () => {
                 id="statement3"
                 className="w-4 h-4 ml-1 mr-2 cursor-pointer"
               />
-              <label htmlFor="statement3">Statement 3</label>
+              <div className="w-fit">
+                <label htmlFor="statement3">Statement 3</label>
+              </div>
             </div>
           </div>
         </div>
-        <div>
-          <button className="w-full mt-4 text-gray-100 bg-sky-400 hover:bg-sky-700 py-2 transition-colors rounded-md">
+        <div className="md:flex md:justify-end md:h-1/5 md:items-end mt-4 md:mt-0">
+          <button className="w-full md:w-1/5 text-lg text-gray-100 bg-sky-400 hover:bg-sky-700 py-2 transition-all rounded-md">
             Next
           </button>
         </div>
