c8e8cbb44725e66085b0017a25591c8ef0742587
Lasma Poksane <laasmapoksane@gmail.com>
2024-05-07 10:38:55 +0200

    style Header component

--- src/containers/Header.jsx
+++ src/containers/Header.jsx
@@ -3,12 +3,12 @@ import Timer from "./Timer";
 
 const Header = () => {
   return (
-    <header>
-      <div>
-        <div>
+    <header className="bg-sky-400 py-2">
+      <div className="flex mx-3 mx-2">
+        <div className="w-3/6">
           <img src={logo} alt="logo" />
         </div>
-        <div>
+        <div className="w-3/6 flex items-center justify-end">
           <Timer />
         </div>
       </div>
