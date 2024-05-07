72acce6966b47ebaab9fe419c6d18e2f45d88102
Lasma Poksane <laasmapoksane@gmail.com>
2024-05-07 21:12:47 +0200

    fetch test questions and pass props to QuizCard to display fetched data

--- src/components/QuizCard.jsx
+++ src/components/QuizCard.jsx
@@ -1,6 +1,6 @@
 import image from "../assets/view.jpg";
 
-const QuizCard = () => {
+const QuizCard = ({ practiceQuestions }) => {
   return (
     <div className="w-11/12 mt-3 md:flex max-w-7xl">
       <div className="md:w-1/2">
@@ -8,42 +8,53 @@ const QuizCard = () => {
       </div>
       <form className="md:w-1/2">
         <div className="md:h-4/5">
-          <h2 className="text-xl px-2 pt-1 pb-2">Question</h2>
+          <h2 className="text-xl text-gray-950 px-2 pt-1">
+            {practiceQuestions.test_question}
+          </h2>
+          <p className="text-lg text-gray-950 px-2 pt-1 pb-2">
+            {practiceQuestions.test_question_description}
+          </p>
           <div className="flex flex-col gap-1">
-            <div className="text-lg px-1 flex items-center">
+            <div className="px-1 flex items-center">
               <input
                 type="checkbox"
                 id="statement1"
                 className="w-4 h-4 ml-1 mr-2 cursor-pointer"
               />
               <div className="w-fit">
-                <label htmlFor="statement1">Statement 1</label>
+                <label htmlFor="statement1">
+                  {practiceQuestions.test_statement_1}
+                </label>
               </div>
             </div>
-            <div className="text-lg px-1 flex items-center">
+            <div className="px-1 flex items-center">
               <input
                 type="checkbox"
                 id="statement2"
                 className="w-4 h-4 ml-1 mr-2 cursor-pointer"
               />
               <div className="w-fit">
-                <label htmlFor="statement2">Statement 2</label>
+                <label htmlFor="statement2">
+                  {practiceQuestions.test_statement_2}
+                </label>
               </div>
             </div>
-            <div className="text-lg px-1 flex items-center">
+            <div className="px-1 flex items-center">
               <input
                 type="checkbox"
                 id="statement3"
                 className="w-4 h-4 ml-1 mr-2 cursor-pointer"
               />
               <div className="w-fit">
-                <label htmlFor="statement3">Statement 3</label>
+                <label htmlFor="statement3">
+                  {practiceQuestions.test_statement_3}
+                </label>
               </div>
             </div>
           </div>
         </div>
         <div className="md:flex md:justify-end md:h-1/5 md:items-end mt-4 md:mt-0">
-          <button className="w-full md:w-1/5 text-lg text-gray-100 bg-sky-400 hover:bg-sky-700 py-2 transition-all rounded-md">
+          <button className="w-full md:w-1/5 text-gray-100 bg-sky-400 hover:bg-sky-700 py-2 transition-all rounded-md">
             Next
           </button>
         </div>
--- src/containers/Quiz.jsx
+++ src/containers/Quiz.jsx
@@ -1,12 +1,35 @@
 import QuizCard from "../components/QuizCard";
 import Header from "./Header";
+import { useState, useEffect } from "react";
 
 const Quiz = () => {
+  const [practiceQuestions, setPracticeQuestions] = useState([]);
+
+  useEffect(() => {
+    const fetchPracticeQuestions = async () => {
+      try {
+        const response = await fetch(
+          "http://localhost:3000/practice-questions"
+        );
+        if (response.ok) {
+          const data = await response.json();
+          setPracticeQuestions(data);
+        } else {
+          console.error("Failed to fetch practice questions");
+        }
+      } catch (error) {
+        console.error("Error fetching practice questions:", error.message);
+      }
+    };
+
+    fetchPracticeQuestions();
+  }, []);
+
   return (
     <>
       <Header />
       <div className="w-full flex justify-center">
-        <QuizCard />
+        <QuizCard practiceQuestions={practiceQuestions} />
       </div>
     </>
   );
