9f0ee15663c53cf43f2303978d61457c4f29ff4f
Lasma Poksane <laasmapoksane@gmail.com>
2024-05-23 01:24:41 +0200

    adjust styles to Header due to having a new logo

--- src/containers/Header.jsx
+++ src/containers/Header.jsx
@@ -8,15 +8,15 @@ const Header = ({
   totalQuestions,
 }) => {
   return (
-    <header className="bg-sky-400 py-2">
-      <div className="flex mx-3 md:w-11/12 md:mx-auto">
-        <div className="w-3/6 md:w-1/6 flex items-center">
-          <img src={logo} alt="logo" />
+    <header className="w-full bg-sky-400 py-2">
+      <div className="flex w-11/12 items-center justify-center mx-auto">
+        <div className="w-3/6 flex items-center justify-start">
+          <img src={logo} alt="logo" className="w-10 h-10" />
         </div>
-        <div className="w-3/6 md:w-5/6 flex items-center justify-end">
+        <div className="w-3/6 flex items-center justify-end">
           {quizStarted && (
             <span className="text-gray-950 text-lg px-4">
-              {currentQuestionIndex + 1}/{totalQuestions}
+              Question {currentQuestionIndex + 1} out of {totalQuestions}
             </span>
           )}
           <Timer quizStarted={quizStarted} quizFinished={quizFinished} />
