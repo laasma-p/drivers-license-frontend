479645054d2c6ccf867d782a604a2ba13cebcb9c
Lasma Poksane <laasmapoksane@gmail.com>
2024-05-26 13:41:31 +0200

    remove displaying mistakes code

--- src/containers/Results.jsx
+++ src/containers/Results.jsx
@@ -1,9 +1,4 @@
-const Results = ({
-  correctQuestions,
-  totalQuestions,
-  hasPassed,
-  incorrectQuestions,
-}) => {
+const Results = ({ correctQuestions, totalQuestions, hasPassed }) => {
   return (
     <div className="flex flex-col mx-auto px-8 pt-8 sm:pt-20 md:max-w-xl lg:max-w-2xl xl:max-w-3xl">
       <h1 className="text-xl text-gray-950">
@@ -15,19 +10,6 @@ const Results = ({
           ? "You have passed the test."
           : "You have not passed the test."}
       </p>
-      <p className="w-full mb-4 text-gray-950">See the mistakes below:</p>
-      <div className="flex flex-row flex-wrap gap-2">
-        {incorrectQuestions.map((index) => {
-          return (
-            <div
-              key={index}
-              className="w-16 h-16 border-2 border-red-200 text-gray-950 hover:text-red-200 flex text-2xl font-semibold justify-center items-center cursor-pointer transition-all"
-            >
-              {index + 1}
-            </div>
-          );
-        })}
-      </div>
     </div>
   );
 };
