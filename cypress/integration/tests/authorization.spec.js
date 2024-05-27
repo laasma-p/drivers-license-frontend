dce559a1e7de7f3312d72105be90091712f400c0
Lasma Poksane <laasmapoksane@gmail.com>
2024-05-27 03:26:05 +0200

    add second test

--- cypress/integration/tests/authorization.spec.js
+++ cypress/integration/tests/authorization.spec.js
@@ -7,4 +7,10 @@ describe("Authorization", () => {
     cy.get("input#code").should("exist");
     cy.contains("button", "Continue").should("exist");
   });
+
+  it("has to update enteredCode state when user enters the code", () => {
+    const code = "2kaei3";
+    cy.get("input#code").type(code);
+    cy.get("input#code").should("have.value", code);
+  });
 });
