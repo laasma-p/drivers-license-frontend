8980fe94c6a432f96e4d1832f53519e8c3f07a5a
Lasma Poksane <laasmapoksane@gmail.com>
2024-05-28 09:58:54 +0200

    re-do test for Quiz and test it's visibility

--- cypress/integration/tests/quiz.spec.js
+++ cypress/integration/tests/quiz.spec.js
@@ -1,5 +1,5 @@
 describe("Quiz", () => {
-  it("has to render authorization and instructions before rendering Quiz", () => {
+  beforeEach(() => {
     cy.visit("/");
     cy.intercept("POST", "http://localhost:3000/verify-code", {
       statusCode: 200,
@@ -9,7 +9,15 @@ describe("Quiz", () => {
     cy.get("input#code").type("35jrwj");
     cy.contains("button", "Continue").click();
 
+    cy.wait("@verifyCode");
+
     cy.contains("Welcome to the theory exam.").should("exist");
     cy.contains("button", "Continue").click();
+  });
+
+  it("has to render Quiz and it's sub-components", () => {
+    cy.get("[data-testid='quiz']").should("exist");
+    cy.get("[data-testid='quiz-card']").should("exist");
+    cy.get("[data-testid='header']").should("exist");
   });
 });
