d165f5fc0e8a1ece3acc7f79cff12d8825bfae62
Lasma Poksane <laasmapoksane@gmail.com>
2024-05-28 10:25:32 +0200

    re-do QuizCard testing and test if practice questions are correctly marked and questions are changed

--- cypress/integration/tests/quiz-card.spec.js
+++ cypress/integration/tests/quiz-card.spec.js
@@ -1,5 +1,5 @@
 describe("QuizCard", () => {
-  it("has to render authorization and instructions before rendering Quiz", () => {
+  beforeEach(() => {
     cy.visit("/");
     cy.intercept("POST", "http://localhost:3000/verify-code", {
       statusCode: 200,
@@ -9,7 +9,19 @@ describe("QuizCard", () => {
     cy.get("input#code").type("35jrwj");
     cy.contains("button", "Continue").click();
 
+    cy.wait("@verifyCode");
+
     cy.contains("Welcome to the theory exam.").should("exist");
     cy.contains("button", "Continue").click();
+  });
+
+  it("has to navigate through practice questions", () => {
+    cy.get("[data-testid='quiz-card']").should("exist");
+    cy.get("input[type='checkbox']").first().check();
+    cy.contains("button", "Next").should("be.enabled").click();
+
+    cy.get("[data-testid='quiz-card']").should("exist");
+    cy.get("input[type='checkbox']").first().check();
+    cy.contains("button", "Start").should("be.enabled").click();
   });
 });
