10e37d775cd54e7d45fe1fa68eb13c257ece91c6
Lasma Poksane <laasmapoksane@gmail.com>
2024-05-28 10:13:33 +0200

    add configurations for end-to-end testing

--- cypress.config.js
+++ cypress.config.js
@@ -2,8 +2,12 @@ import { defineConfig } from "cypress";
 
 export default defineConfig({
   e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
+    baseUrl: "http://192.168.0.101:5173",
+    setupNodeEvents(on, config) {},
+    supportFile: "cypress/support/e2e.js",
+    specPattern: "cypress/integration/tests/*.spec.js",
   },
+
+  viewportWidth: 1366,
+  viewportHeight: 768,
 });
