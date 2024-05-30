c0ffd49155656be397c83af857c965512b59fdb6
Lasma Poksane <laasmapoksane@gmail.com>
2024-05-30 13:32:12 +0200

    add a test that checks that the timer and the quiz is not started when there are no actual test questions

--- cypress/integration/tests/header.spec.js
+++ cypress/integration/tests/header.spec.js
@@ -40,4 +40,36 @@ describe("Header", () => {
     cy.get("[data-testid=timer]").should("exist");
     cy.get("[data-testid=timer]").should("contain", "25:00");
   });
+
+  it("has to not start the timer and the quiz if there are no actual test questions", () => {
+    cy.intercept("GET", "http://localhost:3000/test-questions", {
+      statusCode: 200,
+      body: [],
+    }).as("getTestQuestions");
+
+    cy.visit("/");
+    cy.get("input#code").type("2draB4");
+    cy.contains("button", "Continue").click();
+
+    cy.wait("@verifyCode");
+
+    cy.contains("Welcome to the theory exam.").should("exist");
+    cy.contains("button", "Continue").click();
+
+    cy.get("[data-testid='quiz-card']").should("exist");
+    cy.get("input[type='checkbox']").first().check();
+    cy.contains("button", "Next").should("be.enabled").click();
+
+    cy.get("[data-testid='quiz-card']").should("exist");
+    cy.get("input[type='checkbox']").first().check();
+    cy.contains("button", "Start").should("be.enabled").click();
+
+    cy.wait("@getTestQuestions").then((interception) => {
+      expect(interception.response.body).to.have.lengthOf(0);
+      cy.contains("No test questions available.").should("exist");
+    });
+
+    cy.get("[data-testid='timer']").should("exist");
+    cy.get("[data-testid='timer']").should("contain", "25:00");
+  });
 });
