7ae0afaea6bee767e971ad2dbe2cb96f682646f4
Lasma Poksane <laasmapoksane@gmail.com>
2024-05-30 13:22:45 +0200

    add a test to display the timer on practice questions, but not start it

--- cypress/integration/tests/header.spec.js
+++ cypress/integration/tests/header.spec.js
@@ -19,4 +19,25 @@ describe("Header", () => {
     cy.get("[data-testid='timer']").should("exist");
     cy.get("img").should("exist");
   });
+
+  it("has to display timer for practice questions without starting it", () => {
+    cy.intercept("GET", "http://localhost:3000/practice-questions", {
+      statusCode: 200,
+      body: [],
+    }).as("getPracticeQuestions");
+
+    cy.visit("/");
+    cy.get("input#code").type("2draB4");
+    cy.contains("button", "Continue").click();
+
+    cy.wait("@verifyCode");
+
+    cy.contains("Welcome to the theory exam.").should("exist");
+    cy.contains("button", "Continue").click();
+
+    cy.wait("@getPracticeQuestions");
+
+    cy.get("[data-testid=timer]").should("exist");
+    cy.get("[data-testid=timer]").should("contain", "25:00");
+  });
 });
